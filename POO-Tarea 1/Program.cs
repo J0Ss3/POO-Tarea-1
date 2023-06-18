4f6c6801c9dcaa02e17f11c92d5214b144f00a33
J0Ss3 <jfvargascarrasco03@gmail.com>
2023-06-18 15:07:01 -0600

    Agregar archivos de proyecto.

---
+++ POO-Tarea 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Tarea_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("Hola Mundo");
+            Console.ReadLine(); 
+        }
+    }
+}
